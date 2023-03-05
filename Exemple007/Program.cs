ce50fa8c9f00aa92bf8c6aa3997f71e4bb90589e
NikolaiAbramov <abramovn85@mail.ru>
2023-03-05 19:34:40 +0700

    код

--- Exemple007/Program.cs
+++ Exemple007/Program.cs
@@ -2,8 +2,8 @@
 // Console.SetCursorPosition(10, 4);
 // Console.WriteLine("+");
 
-int xa = 40, ya = 1,
-    xb = 1, yb = 30,
+int xa = 40, ya = 1, 
+    xb = 1, yb = 30, 
     xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
@@ -25,19 +25,19 @@ while (count < 10000)
   int what = new Random().Next(0, 3); // [0;3] 0 1 2
   if (what == 0)
   {
-      x = (x + xa);
+      x = (x + xa) / 2;
       y = (y + ya) / 2;
   }
 
   if (what == 1)
   {
-      x = (x + xb);
+      x = (x + xb) / 2;
       y = (y + yb) / 2;
   }
 
   if (what == 2)
   {
-      x = (x + xc);
+      x = (x + xc) / 2;
       y = (y + yc) / 2;
   }
 
